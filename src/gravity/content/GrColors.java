29574351b7fbb498c8bd0e9ec4f16abb8a461fab
Raviravirego76466 <raviravirego@gmail.com>
2025-06-21 16:33:41 -0300

    Update GrColors.java

--- src/gravity/content/GrColors.java
+++ src/gravity/content/GrColors.java
@@ -2,7 +2,7 @@ package gravity.content;
 
 import arc.graphics.Color;
 
-public class TektonColor {
+public class GrColor {
 	public static Color 
 	gravityColor = Color.valueOf("639bff"),
 	antiGravityColor = Color.valueOf("d95763");
