432cdb7278916329f578f05c4cffefd76c44425c
Raviravirego76466 <raviravirego@gmail.com>
2025-06-21 16:33:08 -0300

    Create GrColors.java

---
+++ src/gravity/content/GrColors.java
@@ -0,0 +1,9 @@
+package tekton.content;
+
+import arc.graphics.Color;
+
+public class TektonColor {
+	public static Color 
+	gravityColor = Color.valueOf("639bff"),
+	antiGravityColor = Color.valueOf("d95763");
+}
