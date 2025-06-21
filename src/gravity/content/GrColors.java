33995712a46566990c9c87e21da36655226a44f4
Raviravirego76466 <raviravirego@gmail.com>
2025-06-21 16:34:01 -0300

    Update GrColors.java

--- src/gravity/content/GrColors.java
+++ src/gravity/content/GrColors.java
@@ -2,7 +2,7 @@ package gravity.content;
 
 import arc.graphics.Color;
 
-public class GrColor {
+public class GrColors {
 	public static Color 
 	gravityColor = Color.valueOf("639bff"),
 	antiGravityColor = Color.valueOf("d95763");
